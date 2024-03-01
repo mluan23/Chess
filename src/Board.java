99468735e34e74329803ce46beed869372f7f193
Matthew Luan <luanmrun20@gmail.com>
2024-02-29 22:43:35 -0500

    Initial commit

---
+++ src/Board.java
@@ -0,0 +1,10 @@
+public class Board {
+    char[][] grid = new char[8][8];
+
+    public Board(){
+
+    }
+    private void resetBoard(){
+        for (int i = 0; i < )
+    }
+}
---
+++ src/Pieces.java
@@ -0,0 +1,8 @@
+public enum Pieces {
+    PAWN,
+    KNIGHT,
+    BISHOP,
+    ROOK,
+    QUEEN,
+    KING
+}
